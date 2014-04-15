f45f0b6a09bc38d10ff362edafde9a23fda2cb58
madflow3 <alexandre.diop@gmail.com>
2014-04-15 16:58:53 +0200

    boostrap t ok

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fdm::Application.config.secret_key_base = '0f5d7e23f4754e3f4e061c3d7bb5a8fc3010ba570d21ccb56d75d8936ce42f6af4ab9fc9a40afbccf1ab844d142996381adc2e6c7236945e6fff3415be5cc95a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fdm::Application.config.session_store :cookie_store, key: '_fdm_session'
